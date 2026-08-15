0dbed808d0c7ab257a6e2017fcc974c6a0c5063d
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-15 14:24:38 -0700

    export generated board projections

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export * from "./castle.ts";
 export * from "./generated.ts";
 export * from "./blake3.ts";
 export * from "./fortune5.ts";
+export * from "./board.generated.ts";
+export * from "./board-goals.generated.ts";
 export * from "./board.ts";
